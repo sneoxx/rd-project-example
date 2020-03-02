1c09aaac16dff212a6059916bdd401b1fc61fe76
Sergei Kornilov <Sergei_Kornilov@epam.com>
2020-03-02 10:29:47 +0400

    Initial commit

---
+++ src/main/java/love/korni/epam/rd/Main.java
@@ -0,0 +1,14 @@
+package love.korni.epam.rd;
+
+/**
+ * Main class
+ *
+ * @author Sergei_Kornilov
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
