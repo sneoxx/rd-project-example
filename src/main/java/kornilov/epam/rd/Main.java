4ac22c066ba7c10a3b6104b55a6da42282f3988e
Sergei Kornilov <Sergei_Kornilov@epam.com>
2020-05-05 01:45:01 +0400

    BAD MSG

--- src/main/java/kornilov/epam/rd/Main.java
+++ src/main/java/kornilov/epam/rd/Main.java
@@ -3,7 +3,7 @@ package kornilov.epam.rd;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world y!");
     }
 
 }
